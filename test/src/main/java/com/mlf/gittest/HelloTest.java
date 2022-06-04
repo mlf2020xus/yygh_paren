e1b74eb9e8610b641beb67e6aef4f83971fc1ee3
tom_glb <goodMorning_glb@mlf.com>
2022-06-04 10:24:09 +0800

    hot-fix commit

---
+++ test/src/main/java/com/mlf/gittest/HelloTest.java
@@ -0,0 +1,14 @@
+package com.mlf.gittest;
+
+
+/**
+ * Created by Administrator on 2022/6/4.
+ */
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("第一个分支");
+        System.out.println("第一个分支");
+        System.out.println("第一个分支");
+        System.out.println("第一个分支");
+    }
+}
